4f5a5bc007a7c34a5843504f2f448dfbe260ae03
mina <minaatef095@gmail.com>
2025-07-20 12:35:44 +0300

    project upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+var btn1 = document.querySelector(".sec3-sp1")
+var btn2 = document.querySelector(".sec3-sp2")
+var btn3 = document.querySelector(".sec3-sp3")
+
+btn1.onclick = function() {
+    alert("Text copied to cliboard")
+}
+btn2.onclick = function() {
+    alert("Text copied to cliboard")
+}
+btn3.onclick = function() {
+    alert("Text copied to cliboard")
+}
